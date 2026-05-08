8127610f5609284253dada7465613cabe916a1d8
ndom91 <yo@ndo.dev>
2026-05-08 19:04:35 +0200

    Update doc width

--- ui/components/App.tsx
+++ ui/components/App.tsx
@@ -248,7 +248,7 @@ export default function App() {
           </aside>
 
           <div className="flex-1 min-w-0 flex justify-center px-4 py-6 pb-24 sm:px-6 lg:px-8">
-            <div className="w-full max-w-250 min-w-0">
+            <div className="w-full max-w-220 min-w-0">
               {/* Document surface */}
               <main id="main-content" tabIndex={-1} className="bg-paper border border-rule rounded-md overflow-hidden">
                 <DocumentChrome
